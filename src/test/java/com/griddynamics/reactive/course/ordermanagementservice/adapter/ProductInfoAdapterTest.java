a717cdbdb41fc3c540c8f20a9a7b41356b3aba4e
DTykhonov <DTykhonov@wsgc.com>
2022-08-15 00:31:24 +0300

    [denshykk] replaced simple mocking with wiremock stubs

--- src/test/java/com/griddynamics/reactive/course/ordermanagementservice/adapter/ProductInfoAdapterTest.java
+++ src/test/java/com/griddynamics/reactive/course/ordermanagementservice/adapter/ProductInfoAdapterTest.java
@@ -1,53 +1,40 @@
 package com.griddynamics.reactive.course.ordermanagementservice.adapter;
 
-import java.nio.file.Files;
-import java.nio.file.Path;
-
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.springframework.http.HttpStatus;
-import org.springframework.web.reactive.function.client.ClientResponse;
-import org.springframework.web.reactive.function.client.WebClient;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cloud.contract.wiremock.AutoConfigureWireMock;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+import com.github.tomakehurst.wiremock.client.WireMock;
 import com.griddynamics.reactive.course.ordermanagementservice.domain.Product;
-import lombok.SneakyThrows;
 import reactor.core.publisher.Flux;
-import reactor.core.publisher.Mono;
 import reactor.test.StepVerifier;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+@SpringBootTest
+@AutoConfigureWireMock(port = 8082)
+@ExtendWith(SpringExtension.class)
 class ProductInfoAdapterTest {
 
-  private static final String PRODUCT_ID     = "111";
-  private static final String PRODUCT_CODE   = "3852";
-  private static final String PRODUCT_NAME   = "IceCream";
-  private static final Double SCORE          = 1689.37;
-  private static final String JSON_FILE_PATH = "/Users/dtykhonov/IdeaProjects/Reactive_Paradigm_FOR_STUDENTS/order" +
-      "-management-service/src/test/resources/product-info-adapter-response.json";
+  private static final String PRODUCT_ID   = "111";
+  private static final String PRODUCT_CODE = "3852";
+  private static final String PRODUCT_NAME = "IceCream";
+  private static final Double SCORE        = 1689.37;
+  private static final String JSON         =
+      "{\"productId\": \"111\", \"productCode\": \"3852\", " + "\"productName\": \"IceCream\", \"score\": 1689.37}";
 
+  @Autowired
   private ProductInfoAdapter productInfoAdapter;
-  private String             json;
-
-  @BeforeEach
-  @SneakyThrows
-  void setUp() {
-    json = Files.readAllLines(Path.of(JSON_FILE_PATH)).get(0);
-
-    WebClient webClient = WebClient
-        .builder()
-        .exchangeFunction(request -> Mono.just(ClientResponse
-            .create(HttpStatus.OK)
-            .header("content-type", "application/json")
-            .body(json)
-            .build()))
-        .build();
-
-    productInfoAdapter = new ProductInfoAdapter(webClient);
-  }
 
   @Test
   void shouldGetProductNamesByProductCode() {
+    WireMock.stubFor(WireMock
+        .get(WireMock.urlMatching("/productInfoService/product/names\\?productCode=" + PRODUCT_CODE))
+        .willReturn(WireMock.aResponse().withHeader("content-type", "application/json").withBody(JSON)));
+
     final Flux<Product> productFlux = productInfoAdapter.getProductNamesByProductCode(PRODUCT_CODE);
 
     StepVerifier.create(productFlux).assertNext(product -> {
--- src/test/java/com/griddynamics/reactive/course/ordermanagementservice/service/UserServiceTest.java
+++ src/test/java/com/griddynamics/reactive/course/ordermanagementservice/service/UserServiceTest.java
@@ -1,17 +1,19 @@
 package com.griddynamics.reactive.course.ordermanagementservice.service;
 
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 
+import com.github.tomakehurst.wiremock.WireMockServer;
+import com.github.tomakehurst.wiremock.client.WireMock;
 import com.griddynamics.reactive.course.ordermanagementservice.adapter.OrderSearchAdapter;
 import com.griddynamics.reactive.course.ordermanagementservice.adapter.ProductInfoAdapter;
-import com.griddynamics.reactive.course.ordermanagementservice.domain.Order;
 import com.griddynamics.reactive.course.ordermanagementservice.domain.OrderInfo;
-import com.griddynamics.reactive.course.ordermanagementservice.domain.Product;
 import com.griddynamics.reactive.course.ordermanagementservice.domain.User;
 import com.griddynamics.reactive.course.ordermanagementservice.repository.UserRepository;
 import reactor.core.publisher.Flux;
@@ -21,6 +23,7 @@ import reactor.test.StepVerifier;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.when;
 
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @ExtendWith(MockitoExtension.class)
 class UserServiceTest {
 
@@ -32,39 +35,53 @@ class UserServiceTest {
   private static final String PRODUCT_CODE = "3852";
   private static final String PRODUCT_ID   = "111";
   private static final String PRODUCT_NAME = "IceCream";
-  private static final Double SCORE        = 1689.37;
+
+  private WireMockServer wireMockServer1 = new WireMockServer(8081);
+  private WireMockServer wireMockServer2 = new WireMockServer(8082);
 
   @Mock
   private UserRepository     userRepository;
-  @Mock
+  @Autowired
   private OrderSearchAdapter orderSearchAdapter;
-  @Mock
+  @Autowired
   private ProductInfoAdapter productInfoAdapter;
-  @InjectMocks
   private UserService        userService;
-
-  private User  user;
-  private Order order;
 
   @BeforeEach
   void setUp() {
-    user = User.builder().id(USER_ID).name(USER_NAME).phone(USER_PHONE).build();
-    order = Order.builder().phoneNumber(USER_PHONE).orderNumber(ORDER_NUMBER).productCode(PRODUCT_CODE).build();
+    wireMockServer1.start();
+    wireMockServer2.start();
+
+    userService = new UserService(userRepository, orderSearchAdapter, productInfoAdapter);
+  }
+
+  @AfterEach
+  void tearDown() {
+    wireMockServer1.stop();
+    wireMockServer2.stop();
   }
 
   @Test
   void shouldGetOrderInfoById() {
-    final Product product = Product
-        .builder()
-        .productId(PRODUCT_ID)
-        .productCode(PRODUCT_CODE)
-        .productName(PRODUCT_NAME)
-        .score(SCORE)
-        .build();
-
+    User user = User.builder().id(USER_ID).name(USER_NAME).phone(USER_PHONE).build();
     when(userRepository.findById(USER_ID)).thenReturn(Mono.just(user));
-    when(orderSearchAdapter.getOrdersByPhone(USER_PHONE)).thenReturn(Flux.just(order));
-    when(productInfoAdapter.getProductNamesByProductCode(PRODUCT_CODE)).thenReturn(Flux.just(product));
+
+    WireMock.configureFor(wireMockServer1.port());
+    WireMock.stubFor(WireMock
+        .get(WireMock.urlMatching("/orderSearchService/order/phone\\?phoneNumber=" + USER_PHONE))
+        .willReturn(WireMock
+            .aResponse()
+            .withHeader("Content-Type", "application/json")
+            .withBody("[{\"orderNumber\":\"Order_0\",\"productCode\":\"3852\"}]")));
+
+    WireMock.configureFor(wireMockServer2.port());
+    WireMock.stubFor(WireMock
+        .get(WireMock.urlMatching("/productInfoService/product/names\\?productCode=" + PRODUCT_CODE))
+        .willReturn(WireMock
+            .aResponse()
+            .withHeader("Content-Type", "application/json")
+            .withBody("{\"productId\":\"111\",\"productCode\":\"3852\",\"productName\":\"IceCream\",\"score\":1689" +
+                ".37}")));
 
     final Flux<OrderInfo> orderInfoFlux = userService.getOrderInfoById(REQUEST_ID, USER_ID);
 
